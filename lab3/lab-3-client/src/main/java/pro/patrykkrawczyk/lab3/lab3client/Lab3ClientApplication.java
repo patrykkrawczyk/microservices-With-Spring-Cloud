1fe0633a63132ef593a268f13f054504b28e51c6
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-13 00:05:56 +0100

    initialized lab-3-client project

---
+++ lab3/lab-3-client/src/main/java/pro/patrykkrawczyk/lab3/lab3client/Lab3ClientApplication.java
@@ -0,0 +1,14 @@
+package pro.patrykkrawczyk.lab3.lab3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3ClientApplication.class, args);
+    }
+
+}
+
