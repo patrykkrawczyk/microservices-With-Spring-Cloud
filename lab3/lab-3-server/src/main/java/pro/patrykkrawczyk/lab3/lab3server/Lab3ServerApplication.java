b40e2ee8564420135ef1b0bcd181b3f083edb2a8
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-13 00:05:35 +0100

    updated server to support git config

--- lab3/lab-3-server/src/main/java/pro/patrykkrawczyk/lab3/lab3server/Lab3ServerApplication.java
+++ lab3/lab-3-server/src/main/java/pro/patrykkrawczyk/lab3/lab3server/Lab3ServerApplication.java
@@ -2,13 +2,14 @@ package pro.patrykkrawczyk.lab3.lab3server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Lab3ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab3ServerApplication.class, args);
     }
-
 }
 
