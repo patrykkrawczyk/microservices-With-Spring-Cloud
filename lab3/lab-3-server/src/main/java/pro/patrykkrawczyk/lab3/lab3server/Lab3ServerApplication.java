c7a51cc231f2ec4486be8f71cae26871f3067c94
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-12 23:39:31 +0100

    initialized lab-3-server project

---
+++ lab3/lab-3-server/src/main/java/pro/patrykkrawczyk/lab3/lab3server/Lab3ServerApplication.java
@@ -0,0 +1,14 @@
+package pro.patrykkrawczyk.lab3.lab3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3ServerApplication.class, args);
+    }
+
+}
+
