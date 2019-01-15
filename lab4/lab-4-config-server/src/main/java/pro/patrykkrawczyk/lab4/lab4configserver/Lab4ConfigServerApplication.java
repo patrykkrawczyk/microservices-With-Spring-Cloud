a74cf9dfda2df8ee3bd2f00fae4927d2a4ff8934
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-16 00:02:00 +0100

    added lab4 bonus

---
+++ lab4/lab-4-config-server/src/main/java/pro/patrykkrawczyk/lab4/lab4configserver/Lab4ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package pro.patrykkrawczyk.lab4.lab4configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab4ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4ConfigServerApplication.class, args);
+    }
+
+}
+
--- lab4/lab-4-sentence/src/main/java/pro/patrykkrawczyk/lab4/lab4sentence/SentenceController.java
+++ lab4/lab-4-sentence/src/main/java/pro/patrykkrawczyk/lab4/lab4sentence/SentenceController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
