d040f75c604c899c356b58e01bf61945cdf97740
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-16 01:27:04 +0100

    reworked lab4 to only need a single client project

---
+++ lab4/lab-4-sentence/src/main/java/pro/patrykkrawczyk/lab4/word/Lab4SentenceApplication.java
@@ -0,0 +1,16 @@
+package pro.patrykkrawczyk.lab4.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Lab4SentenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4SentenceApplication.class, args);
+    }
+
+}
+
