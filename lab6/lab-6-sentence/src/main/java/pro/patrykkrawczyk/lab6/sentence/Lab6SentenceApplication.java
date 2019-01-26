2313394dd1bc3a516c238a21299b613240ad0128
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-26 12:20:08 +0100

    fix for lab6 sentence service

--- lab6/lab-6-sentence/src/main/java/pro/patrykkrawczyk/lab6/sentence/Lab6SentenceApplication.java
+++ lab6/lab-6-sentence/src/main/java/pro/patrykkrawczyk/lab6/sentence/Lab6SentenceApplication.java
@@ -3,10 +3,7 @@ package pro.patrykkrawczyk.lab6.sentence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,12 +12,6 @@ public class Lab6SentenceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab6SentenceApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
 
