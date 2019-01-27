bca1685777730372ffa181fbe0c27577572f25bc
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-01-27 18:28:30 +0100

    finished lab8 project

--- lab8/lab-8-word/src/main/java/pro/patrykkrawczyk/lab8/word/controller/WordController.java
+++ lab8/lab-8-word/src/main/java/pro/patrykkrawczyk/lab8/word/controller/WordController.java
@@ -1,6 +1,7 @@
 package pro.patrykkrawczyk.lab8.word.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.patrykkrawczyk.lab8.word.domain.Word;
@@ -8,6 +9,7 @@ import pro.patrykkrawczyk.lab8.word.domain.Word;
 import java.util.Random;
 
 @RestController
+@RefreshScope
 public class WordController {
 
     private final Random random = new Random();
@@ -19,5 +21,13 @@ public class WordController {
     public Word getWord() {
         String word = words[random.nextInt(words.length)];
         return new Word(word);
+    }
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
     }
 }
