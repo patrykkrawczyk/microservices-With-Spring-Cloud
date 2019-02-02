a0debd522b3fe2aa14e0104a28499381b78641e2
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-02-02 20:59:15 +0100

    added lab9 etag support

--- lab9/lab-9-gateway/src/main/java/pro/patrykkrawczyk/lab9/gateway/Lab9GatewayApplication.java
+++ lab9/lab-9-gateway/src/main/java/pro/patrykkrawczyk/lab9/gateway/Lab9GatewayApplication.java
@@ -3,6 +3,8 @@ package pro.patrykkrawczyk.lab9.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,6 +12,11 @@ public class Lab9GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab9GatewayApplication.class, args);
+    }
+
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
 
